52eaf78d5d4d971d76bbe319ad38e5b5448a2f2c
Vitalii <zhukov-vit@yandex.ru>
2018-05-31 00:12:28 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Carousel from './Components/Carousel';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Carousel />
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ src/Components/Carousel/index.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+// import PropTypes from 'prop-types';
+
+export class Carousel extends PureComponent {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        { 'carousel' }
+      </div>
+    )
+  }
+};
+
+export default Carousel;
